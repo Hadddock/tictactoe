0657b34462149fa9f6000c9eeb1e92daf097ecd8
Haddock <jhadddock@protonmail.com>
2023-03-07 14:03:49 -0800

    Add gameBoard and game modules

---
+++ app.js
@@ -0,0 +1,34 @@
+//gameBoard module
+const gameBoard = (() => {
+  let board = [];
+  const reset = () => {
+    board = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+  };
+  const placePiece = (marker, position) => {
+    if (
+      marker !== " " &&
+      position < board.length &&
+      position >= 0 &&
+      board[position] === " "
+    ) {
+      board[position] = marker;
+      return true;
+    }
+    return false;
+  };
+  const getBoard = () => [...board];
+  reset();
+
+  return { reset, getBoard, placePiece };
+})();
+
+//game module
+const game = (() => {
+  const playerOne = Player("Player One", "X");
+  const playerTwo = Player("Player Two", "O");
+  const board = gameBoard;
+  const playRound = () => {};
+  return { playRound };
+})();
+
+game.play();
