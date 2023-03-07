55426e82284927eab793c4582dd29cd00d450981
Haddock <jhadddock@protonmail.com>
2023-03-07 14:47:17 -0800

    Add checks for gameFinished

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-destructuring */
 //gameBoard module
 const gameBoard = (() => {
   let board = [];
@@ -31,11 +32,42 @@ const Player = (playerName, playerMarker) => {
 
 //game module
 const game = (() => {
-  const playerOne = Player("Player One", "X");
-  const playerTwo = Player("Player Two", "O");
+  let playerOne = Player("Player One", "X");
+  let playerTwo = Player("Player Two", "O");
+  let winner;
   const board = gameBoard;
-  const playRound = () => {};
-  return { playRound };
+  const play = () => {};
+  const checkThree = (positionOne, positionTwo, positionThree) => {
+    const currentBoard = board.getBoard();
+    return (
+      (currentBoard[positionOne] === currentBoard[positionTwo]) ===
+      currentBoard[positionThree]
+    );
+  };
+
+  //return winningPlayer if finished, otherwise false
+  const gameFinished = () => {
+    let currentBoard = board.getBoard();
+    let winningPlayerMarker = "";
+    //win involving first piece
+    if (checkThree(0, 1, 2) || checkThree(0, 3, 6) || checkThree(0, 4, 8)) {
+      winningPlayerMarker = currentBoard[0];
+    } else if (
+      checkThree(1, 4, 7) ||
+      checkThree(3, 4, 5) ||
+      checkThree(6, 4, 2)
+    ) {
+      winningPlayerMarker = currentBoard[4];
+    } else if (checkThree(6, 7, 8) || checkThree(2, 5, 8)) {
+      winningPlayerMarker = currentBoard[8];
+    } else {
+      return false;
+    }
+    if (winningPlayerMarker === playerOne.marker) {
+      return playerOne;
+    }
+    return playerTwo;
+  };
 })();
 
 game.play();
