26b415965b9984c29372938d8231bf3470fb7bd1
Haddock <jhadddock@protonmail.com>
2023-03-07 18:47:24 -0800

    Add game reseting

--- app.js
+++ app.js
@@ -49,30 +49,31 @@ const game = (() => {
     );
   };
 
+  const reset = () => {
+    winner = undefined;
+    board.reset();
+    currentPlayer = playerOne;
+  };
+
   const gameFinished = () => {
     const currentBoard = board.getBoard();
-    let winningPlayerMarker = "";
-
-    if (checkThree(0, 1, 2) || checkThree(0, 3, 6) || checkThree(0, 4, 8)) {
-      winningPlayerMarker = currentBoard[0];
-    } else if (
+    if (
+      checkThree(0, 1, 2) ||
+      checkThree(0, 3, 6) ||
+      checkThree(0, 4, 8) ||
       checkThree(1, 4, 7) ||
       checkThree(3, 4, 5) ||
-      checkThree(6, 4, 2)
+      checkThree(6, 4, 2) ||
+      checkThree(6, 7, 8) ||
+      checkThree(2, 5, 8)
     ) {
-      winningPlayerMarker = currentBoard[4];
-    } else if (checkThree(6, 7, 8) || checkThree(2, 5, 8)) {
-      winningPlayerMarker = currentBoard[8];
-    } else if (!currentBoard.includes("")) {
+      winner = currentPlayer;
       return true;
-    } else {
-      return false;
     }
-    winner = playerTwo;
-    if (winningPlayerMarker === playerOne.marker) {
-      winner = playerOne;
+    if (!currentBoard.includes("")) {
+      return true;
     }
-    return true;
+    return false;
   };
 
   const getPosition = () => {
@@ -83,64 +84,77 @@ const game = (() => {
     return response;
   };
 
-  const play = () => {
-    console.log(board.displayBoard());
-    while (!gameFinished()) {
-      let playerPlaced = false;
-      while (!playerPlaced) {
-        playerPlaced = board.placePiece(getPosition(), playerOne.marker);
-      }
+  // const play = () => {
+  //   console.log(board.displayBoard());
+  //   while (!gameFinished()) {
+  //     let playerPlaced = false;
+  //     while (!playerPlaced) {
+  //       playerPlaced = board.placePiece(getPosition(), playerOne.marker);
+  //     }
 
-      if (gameFinished()) {
-        break;
-      }
-      console.log(board.displayBoard());
+  //     if (gameFinished()) {
+  //       break;
+  //     }
+  //     console.log(board.displayBoard());
 
-      playerPlaced = false;
-      while (!playerPlaced) {
-        playerPlaced = board.placePiece(getPosition(), playerTwo.marker);
-      }
-      console.log(board.displayBoard());
-    }
-    console.log(board.displayBoard());
-    if (winner === undefined) {
-      console.log("Tie Game!");
-    } else {
-      console.log(`${winner.name} Wins!`);
-    }
-  };
-
-  const playRound = (position) => {
-    board.placePiece(position, currentPlayer.marker);
-    currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
-    if (gameFinished()) {
-      if (winner === undefined) {
-        console.log("Tie Game!");
-      } else {
-        console.log(`${winner.name} Wins!`);
-      }
-    }
-  };
+  //     playerPlaced = false;
+  //     while (!playerPlaced) {
+  //       playerPlaced = board.placePiece(getPosition(), playerTwo.marker);
+  //     }
+  //     console.log(board.displayBoard());
+  //   }
+  //   console.log(board.displayBoard());
+  //   if (winner === undefined) {
+  //     console.log("Tie Game!");
+  //   } else {
+  //     console.log(`${winner.name} Wins!`);
+  //   }
+  // };
 
   const getCurrentPlayer = () =>
     Player(currentPlayer.name, currentPlayer.marker);
 
-  return { playRound, getCurrentPlayer, gameFinished };
+  const playRound = (position) => {
+    board.placePiece(position, currentPlayer.marker);
+
+    if (gameFinished()) {
+      if (winner === undefined) {
+        console.log("Tie Game!");
+      } else {
+        console.log(`${currentPlayer.name} Wins!`);
+      }
+    }
+    currentPlayer = currentPlayer === playerTwo ? playerOne : playerTwo;
+  };
+
+  return { playRound, getCurrentPlayer, gameFinished, reset };
 })();
 
 const htmlBoard = document.getElementById("board");
 
+const resetButton = document.getElementById("reset");
+
 function callPlayRound(e) {
   if (e.target.textContent === "" && !game.gameFinished()) {
-    game.playRound(e.target.id);
-    console.log("blank space touched");
     e.target.textContent = game.getCurrentPlayer().marker;
+    game.playRound(e.target.id);
   }
 }
+const squares = [];
 
 for (let i = 0; i < 9; i += 1) {
   const button = document.createElement("button");
   button.id = i;
   button.addEventListener("click", callPlayRound);
+  squares.push(button);
   htmlBoard.appendChild(button);
 }
+
+function resetBoard() {
+  squares.forEach((square) => {
+    square.textContent = "";
+  });
+}
+
+resetButton.addEventListener("click", game.reset);
+resetButton.addEventListener("click", resetBoard);
